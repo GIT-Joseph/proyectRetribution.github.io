ed068659966e63a75eb1688f7e2b7e9f4686cd2f
GIT-Joseph <zlojpc2001@gmail.com>
2026-03-02 13:53:45 -0500

    Add files via upload
    Segundo Comit de la pagina

--- script.js
+++ script.js
@@ -5,13 +5,19 @@ fetch("data.json")
     document.getElementById("nombre").textContent = data.nombre;
     document.getElementById("descripcion").textContent = data.descripcion;
 
-    const lista = document.getElementById("habilidades");
-
-    data.habilidades.forEach(habilidad => {
-      const li = document.createElement("li");
-      li.textContent = habilidad;
-      lista.appendChild(li);
-    });
-
   })
   .catch(error => console.error("Error cargando JSON:", error));
+
+function redirigir() {
+  const valor = document.getElementById("busqueda").value.trim();
+
+  if (valor !== "") {
+    window.location.href = "resultado.html?nombre=" + encodeURIComponent(valor);
+  }
+}
+
+document.getElementById("busqueda").addEventListener("keypress", function(e) {
+  if (e.key === "Enter") {
+    redirigir();
+  }
+});
